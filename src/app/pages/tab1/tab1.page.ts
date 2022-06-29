5312434f25b162de6e731b1e42600281a72a0e0f
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-06-29 10:24:54 -0500

    :heavy_plus_sign: Add Barcode Scanner

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
-  constructor() {}
+  onScan() {
+    this.barcodeScanner
+      .scan()
+      .then((barcodeData) => {
+        console.log('Barcode data', barcodeData);
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  }
 
+  ionViewDidEnter() {
+    console.log('Enter');
+    this.onScan();
+  }
 }
